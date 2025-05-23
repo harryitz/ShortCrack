b4f162036b43ef901d025b7c3c5778eec586c3ae
harryitz <nguyenhieu.c47s@gmail.com>
2025-05-23 18:38:30 +0700

    fix: off extension not disable bypasses

--- src/content_main.js
+++ src/content_main.js
@@ -1,6 +1,11 @@
 import "./bypass/test.js";
 import { bypassUrl } from "./bypass/registry.js";
+import info from "./shortcrack.js";
 
 export async function main() {
-    const bypass = await bypassUrl(window.location.href);
+    const dev_mode_enabled = (await chrome.storage.local.get(["dev_mode_enabled"]).dev_mode_enabled) ?? false;
+
+    if (info.is_development && dev_mode_enabled) {
+        const bypass = await bypassUrl(window.location.href);
+    }
 }
